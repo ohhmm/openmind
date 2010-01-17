c6d0f6995c30b23994dfeed324a32e9c9f893de0
unknown <name@.(none)>
2010-01-17 19:52:20 +0200

    Changing goals container type

--- OpenMind/Mind.cpp
+++ OpenMind/Mind.cpp
@@ -16,33 +16,13 @@ void Mind::Run()
 {
 	do
 	{
-		bool someRiched = false;
-		//BOOST_FOREACH(Goal::ptr_t goal, goals_)
-		for(goals_collection_t::iterator it = goals_.begin(); it != goals_.end(); ++it)
+		if (!ReachGoals())
 		{
-			Goal::ptr_t goal = *it;
-			bool isRiched = std::find(richedGoals_.begin(), richedGoals_.end(), goal) != richedGoals_.end();
-			someRiched = isRiched || goal->Rich();
-			if (someRiched)
+			BOOST_FOREACH(GoalGenerator::ptr_t generator, goalGenerators_)
 			{
-				if (!isRiched) // it is riched by now
-				{
-					richedGoals_.insert( richedGoals_.end(), goal);
-				}
-
-				goals_.erase( it );
+				goals_.push_back(
+					generator->GenerateGoal() );
 			}
-			else
-			{
-				Goal::container_t newGoals = goal->ToRich();
-				goals_.insert( goals_.end(), newGoals.begin(), newGoals.end() ); 
-			}
-		}
-
-		BOOST_FOREACH(GoalGenerator::ptr_t generator, goalGenerators_)
-		{
-			goals_.push_back(
-				generator->GenerateGoal() );
 		}
 	}
 	while (!goals_.empty());
@@ -54,4 +34,36 @@ void Mind::AddGoalGenerator( GoalGenerator::ptr_t generator )
 		boost::bind(&goals_collection_t::push_back, goals_, _1 ));
 
 	goalGenerators_.insert(generator);
+}
+
+bool Mind::ReachGoals()
+{
+	bool someRiched = false;
+
+	if (goals_.size())
+	{
+		for( goals_collection_t::iterator it = goals_.begin();
+			it != goals_.end(); ++it )
+		{
+			Goal::ptr_t goal = *it;
+			bool isRiched = std::find(richedGoals_.begin(), richedGoals_.end(), goal) != richedGoals_.end();
+			someRiched = isRiched || goal->Rich();
+			if (someRiched)
+			{
+				if (!isRiched) // it is riched by now
+				{
+					richedGoals_.insert(richedGoals_.begin(), it, it);
+				}
+
+				goals_.erase( it );
+			}
+			else
+			{
+				Goal::container_t newGoals = goal->ToRich();
+				goals_.insert( it, newGoals.begin(), newGoals.end() ); 
+			}
+		}
+	}
+
+	return someRiched;
 }
--- OpenMind/Mind.h
+++ OpenMind/Mind.h
@@ -6,12 +6,14 @@
 class Mind
 {
 	std::set<GoalGenerator::ptr_t> goalGenerators_;
-	typedef std::deque<Goal::ptr_t>	goals_collection_t;
+	typedef std::list<Goal::ptr_t>	goals_collection_t;
 	goals_collection_t goals_;
 	goals_collection_t richedGoals_;
 public:
 	Mind(void);
 	void AddGoalGenerator(GoalGenerator::ptr_t generator);
 	void Run();
+
+	bool ReachGoals();
 	~Mind(void);
 };
