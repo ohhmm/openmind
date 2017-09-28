70db8f6cf22f02a278fcfee42a547d2a00f98fc8
Sergei Krivonos <sergeikrivonos@gmail.com>
2017-09-28 22:31:14 +0300

    Update Sum_test.cpp

--- omnn/extrapolator/test/Sum_test.cpp
+++ omnn/extrapolator/test/Sum_test.cpp
@@ -6,16 +6,14 @@
 #include "Variable.h"
 
 BOOST_AUTO_TEST_CASE(Sum_tests)
-{ё
+{
     using s_t = omnn::extrapolator::Sum;
-	using f_t = omnn::extrapolator::Fraction;
-	omnn::extrapolator::Variable v;
-	f_t f(1 , 2);
-	f_t f1(5, 6);
+    using f_t = omnn::extrapolator::Fraction;
+    omnn::extrapolator::Variable v;
+    f_t f(1 , 2);
+    f_t f1(5, 6);
     s_t a(1,2, f, 3) ;
-	s_t b(6, f1);
-	a -= b;
-	std::cout << a;
-
-
+    s_t b(6, f1);
+    a -= b;
+    std::cout << a;
 }
