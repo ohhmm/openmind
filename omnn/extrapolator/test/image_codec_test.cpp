abec43d5b3f4104799bca09d711d30fe0cbfddc9
Sergei Krivonos <sergeikrivonos@gmail.com>
2018-01-05 12:32:19 +0200

    add resized to 20x20 TGA

--- omnn/extrapolator/test/image_codec_test.cpp
+++ omnn/extrapolator/test/image_codec_test.cpp
@@ -6,8 +6,8 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <boost/gil/extension/io/bmp_all.hpp>
 #include <boost/gil/extension/io/targa_all.hpp>
-#include <boost/gil/gil_all.hpp>
 #include "Extrapolator.h"
 
 using namespace omnn::extrapolator;
@@ -25,22 +25,28 @@ std::string l(const omnn::extrapolator::Valuable& v)
 
 BOOST_AUTO_TEST_CASE(ImageCodec_test)
 {
-    rgb8_image_t src;
-    read_image(TEST_SRC_DIR "g.tga", src, targa_tag());
+    rgba8_image_t src;
+    read_image(TEST_SRC_DIR "gresized.tga", src, targa_tag());
     
     auto rows = src.dimensions().y;
     auto cols = src.dimensions().x;
     Extrapolator ex(rows, cols);
+    Extrapolator a(rows, cols);
+    Extrapolator r(rows, cols);
+    Extrapolator g(rows, cols);
+    Extrapolator b(rows, cols);
+
     auto v = view(src);
-    auto b = v.begin();
     for (auto i=ex.size1(); i--;) { // raw
-        for (auto j = ex.size2(); j--;) { // column
-            ex.insert_element(i,j, b->operator[](0));
-            ++b;
+        for (auto j = ex.size2(); j--;) {// column
+            auto px = v(i,j);
+            a(i,j) = px[0];
+            r(i,j) = px[1];
+            g(i,j) = px[2];
+            b(i,j) = px[3];
+            
         }
     }
-    bool iterated = b==v.end();
-    BOOST_TEST(iterated);
     
     Variable x, y, z;
     auto f = ex.Factors(y, x, z);
@@ -53,7 +59,6 @@ BOOST_AUTO_TEST_CASE(ImageCodec_test)
     // inbound data deduce
     rgb8_image_t dst(src.dimensions());
     auto dv = view(dst);
-    b = dv.begin();
     for (auto i=ex.size1(); i--;) { // raw
         for (auto j = ex.size2(); j--;) { // column
             auto c = fo(i, j);
