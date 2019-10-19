0e39e75c13553149f23a198a06cdf8dae61ee601
Sergei Krivonos <sergeikrivonos@gmail.com>
2019-10-19 19:35:23 +0300

    caching in progress

--- omnn/math/Cache.h
+++ omnn/math/Cache.h
@@ -1,32 +1,71 @@
 #pragma once
+
+//#ifdef OPENMIND_MATH_USE_LEVELDB_CACHE
+
 #include <leveldb/db.h>
+#include <memory>
+#include <string>
 
 namespace omnn::math {
-    template<const char* TName>
-    class Data
-    {
-        static leveldb::DB* db;
-        static leveldb::DB* DbOpen() {
-            leveldb::Options options;
-            options.create_if_missing = true;
-            if (leveldb::DB::Open(options, TName, &db).ok())
-                atexit([]() {delete db});
-            return db;
-        }
-    protected:
-        std::string Value(const std::string& key);
-    };
-
-    template<const char* TName>
-    leveldb::DB* Data<TName>::db = DbOpen();
-
-    template<const char* TName>
+    
+    
     class Cache
-        : public Data<TName>
     {
     public:
-        std::shared_ptr<Valuable> Get(const Valuable& va) {
-            return Valuable(Value(va.str(), {}));
+        using path_str_t = std::string;
+        
+    private:
+        leveldb::DB* db = nullptr;
+        path_str_t path;
+        
+        void DbOpen() {
+            leveldb::Options options;
+            options.create_if_missing = true;
+            options.compression = leveldb::kSnappyCompression;
+            
+            if (!leveldb::DB::Open(options, path.c_str(), &db).ok())
+                throw path + " db connection error";
         }
+        
+    protected:
+        std::string Value(const std::string& key);
+        
+    public:
+        Cache(const path_str_t& path)
+        {
+            this->path = path;
+            DbOpen();
+        }
+        
+        ~Cache() {
+            if(db)
+                delete db; // hopely it has overloaded operator delete inside its image
+        }
+        
+        std::pair<bool, Valuable> GetOne(const std::string& key)
+        {
+            std::unique_ptr<leveldb::Iterator> it(db->NewIterator({}));
+            if(it){
+                it->Seek(key);
+                if (it->Valid() && it->key().ToString() == key)
+                    return { true, Valuable(it->value().ToString(), {})};
+            }
+            return {{},{}};
+        }
+        
+//
+//        auto Get(const Valuable& key)
+//        {
+//            std::set<Valuable> values;
+//            auto it = db->NewIterator({});
+//            for (it->Seek(start);
+//                 it->Valid() && it->key() == key.str();
+//                 it->Next()) {
+//            return values;
+//        }
+            
+            
     };
 }
+
+//#endif
