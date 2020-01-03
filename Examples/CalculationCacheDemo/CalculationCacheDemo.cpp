0ff51212d8e6154e97681dd330a493c3ca718e3f
Sergei Krivonos <sergeikrivonos@gmail.com>
2020-01-03 20:45:42 +0200

    #TurkishAirport  fixed CalculationCacheDemo

--- Examples/CalculationCacheDemo/CalculationCacheDemo.cpp
+++ Examples/CalculationCacheDemo/CalculationCacheDemo.cpp
@@ -16,8 +16,11 @@ int main()
   do{
     equation.logic_or(x.Equals(++i));
     std::cout
-      << "solving " << equation << std::endl
-      << equation(x) << std::endl;
+      << "solving " << equation << std::endl;
+    for(auto& s : equation.Solutions()){
+      std::cout << s << ' ';
+    }
+    std::cout << std::endl;
     current_time = std::chrono::high_resolution_clock::now();
   }while(std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time).count() < 60 );
   return 0;
