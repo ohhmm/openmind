4917dbc0aa7f1f12ec5855b5d5e09d20485c3e30
Serg Creevanose <sergeikrivonos@gmail.com>
2020-05-22 05:18:19 +0300

    Made Valuable::operator! to use this->getVarHost() to create 'WhyNot'

--- omnn/math/VarHost.cpp
+++ omnn/math/VarHost.cpp
@@ -6,14 +6,25 @@
  */
 
 #include "VarHost.h"
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
 #include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
 
 using namespace omnn::math;
+
 
 template<>
 any::any TypedVarHost<std::string>::NewVarId()
 {
-    return boost::uuids::uuid();
+    static boost::uuids::random_generator UuidGen;
+    auto id = UuidGen();
+    auto s = boost::lexical_cast<std::string>(id);
+    boost::erase_all(s, "-");
+    static std::string prefix = "va_";
+    s.insert(0, prefix);
+    return std::move(s);
 }
 
 template<>
