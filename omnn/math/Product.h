821454efa525bbf068a2b91f2741d190d03dc347
Serg Krivonos <sergeikrivonos@gmaiil.com>
2021-05-10 18:26:15 +0300

    Some of the new 'Valuable::solutions_t Distinct() const override' implementations

--- omnn/math/Product.h
+++ omnn/math/Product.h
@@ -83,7 +83,7 @@ public:
     Valuable operator()(const Variable& va) const override;
     Valuable operator()(const Variable&, const Valuable& augmentation) const override;
     void solve(const Variable& va, solutions_t& solutions) const override;
-
+    Valuable::solutions_t Distinct() const override;
     
 protected:
     cont& GetCont() override { return members; }
