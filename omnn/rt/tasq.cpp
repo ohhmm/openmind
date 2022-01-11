82c9bf6e452efb033020b67cb1dd07bce56d2676
Sergei Kryvonos <sergeikrivonos@gmail.com>
2022-01-11 12:50:55 +0200

    Prime numbers table. TODO: LZW stream it

--- omnn/rt/tasq.cpp
+++ omnn/rt/tasq.cpp
@@ -2,11 +2,12 @@
 
 #include <iostream>
 
-
 constexpr size_t MaxThreadsForCacheStoring = 1024;
 
 
+
 void StoringTasksQueue::CleanupReadyTasks() {
+    std::lock_guard lg(DequeMutEx);
     while (size() &&
            ((front().valid() &&
              front().wait_for(std::chrono::seconds(0)) ==
@@ -19,13 +20,16 @@ void StoringTasksQueue::CleanupReadyTasks() {
 }
 
 void StoringTasksQueue::AddTask(const std::function<bool()>& f) {
+    
     this->CleanupReadyTasks();
     auto&& task = std::async(std::launch::async, f);
+    std::lock_guard lg(DequeMutEx);
     emplace_back(std::move(task));
 }
 
 void StoringTasksQueue::AddTask(std::function<bool()>&& f) {
     this->CleanupReadyTasks();
     auto&& task = std::async(std::launch::async, std::move(f));
+    std::lock_guard lg(DequeMutEx);
     emplace_back(std::move(task));
 }
