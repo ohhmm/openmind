29858d7b08e48e6b40cda21a35d5e6f69ad116c8
Sergei Kryvonos <sergeikrivonos@gmail.com>
2022-03-20 10:09:59 +0200

    Multi-values

--- omnn/math/test/Fraction_test.cpp
+++ omnn/math/test/Fraction_test.cpp
@@ -58,11 +58,43 @@ BOOST_AUTO_TEST_CASE(Fraction_tests)
     BOOST_TEST(eq);
 }
 
-BOOST_AUTO_TEST_CASE(Fraction_with_sum_tests)
-{
-    auto _ = 841_v/64;
-    auto a = _.Sqrt();
-    _ ^= 1_v/2;
+/// <summary>
+/// See for some reference and description on multivalues origins
+/// </summary>
+BOOST_AUTO_TEST_CASE(Fraction_with_sum_tests) {
+    auto _ = 841_v/64; // 841/64 = 13.140625
+    auto a = _.Sqrt(); // 29/8 = 3.625   - square root stands for principal root (only positive branch).
+	// Branch reduction leads to backward-incompatible expressions which leads to calculation misstakes which is why this framework has limited support for principal root
+
+	// Multivalue expression equivalent:
+    _ ^= 1_v/2; // (841/64)^(1/2) = ?.. lets see possible ways to calculate (covering multivals)
+	// ±29/±8 = ±(29/8) = (1^(1/2))*(29/8) :   
+	// 
+	//  29^2  /  8^2  = 841/64
+    //  29^2 / (-8)^2 = 841/64
+    // (-29)^2 / 8^2  = 841/64
+    // (-29)^2 / (-8)^2 = 841/64
+	//                 <=>
+	//	                  ___    - (841/64)^(1/2) equals some of these values and we do not know which of these exactly for current expression
+	//  (841/64)^(1/2) = | 29/8 = -29/-8 = 3.625
+	//                   | -29/8 = 29/-8 = -3.625
+	//                   \___
+	// 
+	// NOTE: it does not equal to any of these particular values, but it equals this uncertinty, which of these two the value is
+	//       probably the most suitable is to call it the set of these two values:
+	//       (841/64)^(1/2) = ±29/±8 = ±(29/8) = set(-3.625, 3.625)
+	//       mathematical unordered set of values
+	//       multivalue
+	//       this way there is not branch reduction and expressions are backward-compatibly equivalent
+	//   ___                                  ___
+    //  |  29^2  /  8^2  = 841/64            | (29/8)^2 = 841/64              ___
+    //  |  29^2 / (-8)^2 = 841/64            | (29/-8)^2 = 841/64            | 29/8 = -29/-8 = 3.625
+    //  | (-29)^2 / 8^2  = 841/64     <===>  | (-29/8)^2 = 841/64    <===> 	 | -29/8 = 29/-8 = -3.625    <===> (841/64)^(1/2) = ±29/±8 = ±(29/8) = set(-3.625, 3.625)
+    //  | (-29)^2 / (-8)^2 = 841/64          | (-29/-8)^2 = 841/64			 \___
+	//   \__                                  \__
+
+
+
 
     a = (573440_v*(((841_v/64))^((1_v/2))) + 2115584)/262144;
     // (573440 * (±29/±8) + 2115584) / 262144
