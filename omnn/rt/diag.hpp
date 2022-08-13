8a602bcb3990fb5269b200b5b4c3aa6afa251926
Sergio <sergeikrivonos@gmail.com>
2022-08-13 01:08:33 -0700

    Boost 1.80 update

--- omnn/rt/diag.hpp
+++ omnn/rt/diag.hpp
@@ -1,16 +1,17 @@
 
 #include <chrono>
 
-
-template<typename TimeT = std::chrono::nanoseconds>
-struct measure
-{
-    template<typename F, typename ...Args>
-    static typename TimeT::rep execution(F&& func, Args&&... args)
+namespace omnn {
+    template<typename TimeT = std::chrono::nanoseconds>
+    struct measure
     {
-        auto start = std::chrono::steady_clock::now();
-        std::forward<decltype(func)>(func)(std::forward<Args>(args)...);
-        auto duration = std::chrono::duration_cast<TimeT>(std::chrono::steady_clock::now() - start);
-        return duration.count();
-    }
-};
+        template<typename F, typename ...Args>
+        static typename TimeT::rep execution(F&& func, Args&&... args)
+        {
+            auto start = std::chrono::steady_clock::now();
+            std::forward<decltype(func)>(func)(std::forward<Args>(args)...);
+            auto duration = std::chrono::duration_cast<TimeT>(std::chrono::steady_clock::now() - start);
+            return duration.count();
+        }
+    };
+}
