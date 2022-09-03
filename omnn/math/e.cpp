2c12edcf95608acd4a6fc164192376ba787614f5
Sergio <sergeikrivonos@gmail.com>
2022-09-03 00:51:40 -0700

    Product::IsSummationSimplifiable with Sum

--- omnn/math/e.cpp
+++ omnn/math/e.cpp
@@ -11,4 +11,23 @@ constinit std::string_view Euler::SerializationName = "e";
 using namespace constant;
 //    static const Euler e;
 
+namespace {
+auto eSq = Exponentiation{Euler(), 2};
+}
+
+Valuable Euler::Sq() const { return eSq; }
+
+Valuable& Euler::sq() { return Become(Sq()); }
+
+std::pair<bool, Valuable> Euler::IsSummationSimplifiable(const Valuable& v) const {
+	return {};
+}
+
+std::pair<bool, Valuable> Euler::IsMultiplicationSimplifiable(const Valuable& v) const {
+    return {};
+}
+
+bool Euler::MultiplyIfSimplifiable(const Valuable& v) {
+	return {};
+}
 
