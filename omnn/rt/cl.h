4cba025ceb0b116c1c250da7805acad415b6a106
Serg Creevanose <sergeikrivonos@gmail.com>
2022-10-21 14:45:05 -0700

    Support OpenCLless build

--- omnn/rt/cl.h
+++ omnn/rt/cl.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#ifdef OPENMIND_USE_OPENCL
 #include <boost/compute.hpp>
+#else
+namespace boost::compute {
+class device;
+}
+#endif
 
 namespace omnn::rt {
 const boost::compute::device& GetComputeUnitsWinnerDevice();
