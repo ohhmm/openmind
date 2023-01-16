495bc22966a26495ca4c475f616a669c20c842c4
Serg Kryvonos <skryvonos@logitech.com>
2023-01-16 13:51:18 +0200

    Implement time measurement in std::chrono::time_point<std::chrono::steady_clock>

--- omnn/rt/diag.hpp
+++ omnn/rt/diag.hpp
@@ -1,17 +1,24 @@
 
 #include <chrono>
 
-namespace omnn {
-    template<typename TimeT = std::chrono::nanoseconds>
-    struct measure
+namespace omnn {    
+namespace measure {
+
+    template<typename F, typename ...Args>
+    static auto time(F&& func, Args&&... args)
     {
-        template<typename F, typename ...Args>
-        static typename TimeT::rep execution(F&& func, Args&&... args)
-        {
-            auto start = std::chrono::steady_clock::now();
-            std::forward<decltype(func)>(func)(std::forward<Args>(args)...);
-            auto duration = std::chrono::duration_cast<TimeT>(std::chrono::steady_clock::now() - start);
-            return duration.count();
-        }
-    };
-}
+        auto start = std::chrono::steady_clock::now();
+        std::forward<decltype(func)>(func)(std::forward<Args>(args)...);
+        auto duration = std::chrono::steady_clock::now() - start;
+        return duration;
+    }
+
+    template<typename ...Args>
+    static auto nanoseconds(Args&&... args)
+    {
+        auto duration = time(std::forward<Args>(args)...);
+        auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
+        return ns.count();
+    }
+    
+}}
