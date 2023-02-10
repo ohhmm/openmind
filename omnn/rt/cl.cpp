3ee727ecda2a04e0866d5d3ee419dacc43c08c6c
Serg Creevanose <sergeikrivonos@gmail.com>
2023-02-10 08:57:29 -0800

    Resolved dll deps

--- omnn/rt/cl.cpp
+++ omnn/rt/cl.cpp
@@ -2,12 +2,7 @@
 
 #ifdef OPENMIND_USE_OPENCL
 
-#include <boost/archive/binary_oarchive.hpp>
 #include <boost/compute.hpp>
-#include <boost/compute/core.hpp>
-#include <boost/multiprecision/cpp_int.hpp>
-#include <boost/program_options.hpp>
-#include <boost/serialization/vector.hpp>
 
 namespace {
 
@@ -16,20 +11,23 @@ auto ComputeUnitsWinner = []() -> boost::compute::device {
     if (devices.size() == 0) {
         std::cout << "Please, install an OpenCL driver." << std::endl;
         exit(-1);
-    }
-
-    auto cuwinner = boost::compute::system::default_device();
-    for (auto& p : boost::compute::system::platforms()) {
-        for (auto& d : p.devices()) {
-            std::cout << d.name() << ':' << d.compute_units() << std::endl;
-            if (d.compute_units() > cuwinner.compute_units())
-                cuwinner = d;
+    } else if (devices.size() == 1) {
+        auto& device = *devices.begin();
+        std::cout << device.name() << " max_work_group_size=" << device.max_work_group_size() << std::endl;
+        return device;
+    } else {
+        auto cuwinner = boost::compute::system::default_device();
+        for (auto& p : boost::compute::system::platforms()) {
+            for (auto& d : p.devices()) {
+                std::cout << d.name() << ':' << d.compute_units() << std::endl;
+                if (d.compute_units() > cuwinner.compute_units())
+                    cuwinner = d;
+            }
         }
+
+        std::cout << "max_work_group_size: " << cuwinner.max_work_group_size() << std::endl;
+        return cuwinner;
     }
-
-    std::cout << "max_work_group_size: " << cuwinner.max_work_group_size() << std::endl;
-
-    return cuwinner;
 }();
 
 } // namespace
