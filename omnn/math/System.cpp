e16f7958e75435d02b3e17275a5c27a545cf0918
Serg Creevanose <sergeikrivonos@gmail.com>
2023-03-25 02:17:18 -0700

    each.hpp automatically taskable foreach enumerator

--- omnn/math/System.cpp
+++ omnn/math/System.cpp
@@ -12,35 +12,11 @@
 #include <future>
 #include <numeric>
 
-#include <rt/tasq.h>
+#include <rt/each.hpp>
 
 
 using namespace omnn;
 using namespace math;
-
-
-
-template<class T, class F> void peach(T&& c, F&& f) {
-#ifdef __APPLE__
-    StoringTasksQueue().AddTasks(std::forward<T>(c), std::forward<F>(f));
-#else
-    auto b = std::begin(c), e = std::end(c);
-    std::for_each(
-#ifndef __APPLE__
-                  std::execution::par,
-#endif
-                  b, e, f);
-#endif
-}
-
-template <class T, class F> void each(T&& t, F&& f) {
-    if (std::size(t) > 100)
-        peach(std::forward<T>(t), std::forward<F>(f));
-    else {
-        auto b = std::begin(t), e = std::end(t);
-        std::for_each(b, e, f);
-    }
-}
 
 
 bool is_subset(const std::set<std::string>& subs, const std::set<std::string>& set)
