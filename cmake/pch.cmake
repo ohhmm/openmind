1ebe552b69b4642a3421853f7068dc6ca493e83a
Serg Creevanose <sergeikrivonos@gmail.com>
2023-04-30 04:10:39 -0700

    Autoenable MSVC PCH

--- cmake/pch.cmake
+++ cmake/pch.cmake
@@ -2,15 +2,19 @@ MACRO(ADD_MSVC_PRECOMPILED_HEADER PrecompiledHeader PrecompiledSource SourcesVar
   IF(MSVC)
     GET_FILENAME_COMPONENT(PrecompiledBasename ${PrecompiledHeader} NAME_WE)
     SET(PrecompiledBinary "${CMAKE_CURRENT_BINARY_DIR}/${PrecompiledBasename}.pch")
-    SET(Sources ${${SourcesVar}})
 
     SET_SOURCE_FILES_PROPERTIES(${PrecompiledSource}
                                 PROPERTIES COMPILE_FLAGS "/Yc\"${PrecompiledHeader}\" /Fp\"${PrecompiledBinary}\""
                                            OBJECT_OUTPUTS "${PrecompiledBinary}")
-    SET_SOURCE_FILES_PROPERTIES(${Sources}
+
+    list(REMOVE_ITEM SourcesUsingPCH ${${SourcesVar}})
+    SET_SOURCE_FILES_PROPERTIES(${SourcesUsingPCH}
                                 PROPERTIES COMPILE_FLAGS "/Yu\"${PrecompiledBinary}\" /FI\"${PrecompiledBinary}\" /Fp\"${PrecompiledBinary}\""
                                            OBJECT_DEPENDS "${PrecompiledBinary}")  
-    # Add precompiled header to SourcesVar
-    LIST(APPEND ${SourcesVar} ${PrecompiledSource})
+
+    list(FIND SourcesUsingPCH "${${SourcesVar}}" cmpresult)
+    if(NOT cmpresult EQUAL -1)
+      LIST(APPEND ${SourcesVar} ${PrecompiledSource})
+    endif()
   ENDIF(MSVC)
 ENDMACRO(ADD_MSVC_PRECOMPILED_HEADER)
