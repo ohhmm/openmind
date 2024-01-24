aae70cc4ba31af3cb69d2bdb7232a7d26f490241
Serg Creevanose <sergeikrivonos@gmail.com>
2024-01-24 13:50:42 -0800

    Allow task result custom types

--- omnn/rt/tasq.h
+++ omnn/rt/tasq.h
@@ -2,38 +2,95 @@
 #include <deque>
 #include <functional>
 #include <future>
+#include <iostream>
 #include <memory>
 #include <mutex>
+#include <thread>
 #include <utility>
 
 
 namespace omnn::rt {
 
-using StoringTask = std::future<bool>;
-class StoringTasksQueue
-    : public std::deque<StoringTask>
-{
-    std::mutex DequeMutEx;
-
-protected:
-    void CleanupReadyTasks();
+template <class ResultT>
+class Task {
+    std::future<ResultT> task;
+    std::function<void(ResultT&&)> cb;
 
 public:
-    void AddTask(const std::function<bool()>& f);
-    void AddTask(std::function<bool()>&& f);
 
-    template<typename C, typename F>
-    void AddTasks(const C& cont, const std::function<bool()>& f) {
-        for(auto& item : cont)
-            AddTask(f);
+};
+
+template <typename... Args>
+class ParamPackStorage {
+public:
+    // Use std::ref to store references in the constructor
+    ParamPackStorage(Args&&... args)
+        : data(std::forward_as_tuple(args...))
+    {}
+
+    // Access the data using a helper function
+    template <std::size_t I>
+    decltype(auto) get() const {
+        return std::get<I>(data);
     }
 
-    template<typename C, typename F>
-    void AddTasks(C&& cont, F&& f) {
-        for(auto& item : cont) {
-            std::function<bool()> fn = std::bind(std::forward<F>(f), item);
-            AddTask(std::move(fn));
+private:
+    std::tuple<Args...> data;
+};
+
+using StoringTask = std::future<bool>;
+template <typename ResultT = bool>
+class StoringTasksQueue : public std::deque < std::future<ResultT>> {
+    std::mutex DequeMutEx;
+    static constexpr size_t MaxThreadsForCacheStoring = 1024;
+    bool CleanUp;
+
+protected:
+    void CleanupReadyTasks() {
+        if (CleanUp) {
+            bool overburdened = {};
+            do {
+                {
+                    std::lock_guard lg(DequeMutEx);
+                    while (this->size()
+                        && this->front().valid()
+                        && this->front().wait_for(std::chrono::seconds(0)) == std::future_status::ready
+                        ) {
+                        this->pop_front();
+                    }
+                    overburdened = this->size() >= MaxThreadsForCacheStoring;
+                }
+                if (overburdened)
+                    std::this_thread::yield();
+            } while (this->size() >= MaxThreadsForCacheStoring);
         }
+    }
+
+public:
+    StoringTasksQueue(bool autoCleanUp = true)
+        : CleanUp(autoCleanUp)
+    {}
+    
+    template <class FnT, class ...ParamsT>
+    void AddTask(FnT&& f, ParamsT&&... params) {
+        this->CleanupReadyTasks();
+        auto task = std::async(std::launch::async, std::bind(std::forward<FnT>(f), std::forward<ParamsT>(params)...));
+        std::lock_guard lg(DequeMutEx);
+        this->emplace_back(std::move(task));
+    }
+
+    template<typename C, class FnT, class ...ParamsT>
+    void AddIteratorTasks(C&& cont, FnT&& f, ParamsT&&... params) {
+        auto e = cont.end();
+        for (auto i = cont.begin(); i!=e; ++i)
+            AddTask(std::forward<FnT>(f), i, std::forward<ParamsT>(params)...);
+    }
+
+    template<typename C, class FnT, class ...ParamsT>
+    void AddTasks(C&& cont, FnT&& f, ParamsT&&... params) {
+        auto e = cont.end();
+        for (auto i = cont.begin(); i!=e; ++i)
+            AddTask(std::forward<FnT>(f), *i, std::forward<ParamsT>(params)...);
     }
 };
 
