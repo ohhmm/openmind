a8fa83962db71694e4ab10345e8f03f7b6b2dab5
Sergii Kryvonos <sergeikrivonos@gmail.com>
2024-09-19 13:31:22 +0200

    Fix vcpkg use

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -6,32 +6,41 @@ macro(find_vcpkg)
 		        "$ENV{ProgramFiles}/Microsoft Visual Studio/*/*/VC/vcpkg/"
 		        "$ENV{USERPROFILE}$ENV{HOME}/vcpkg/"
 	        )
-	        if (EXISTS ${VCPKG_EXECUTABLE})
-		        get_filename_component(dir ${VCPKG_EXECUTABLE} DIRECTORY)
-		        set(CMAKE_TOOLCHAIN_FILE "${dir}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "Vcpkg toolchain file")
-	        endif ()
         endif()
+		if (EXISTS ${VCPKG_EXECUTABLE})
+			get_filename_component(dir ${VCPKG_EXECUTABLE} DIRECTORY)
+			set(CMAKE_TOOLCHAIN_FILE "${dir}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "Vcpkg toolchain file")
+		endif ()
         if(EXISTS ${CMAKE_TOOLCHAIN_FILE})
             set(VCPKG_FOUND TRUE CACHE BOOL "VCPKG toolchain file found ${CMAKE_TOOLCHAIN_FILE}")
+			list(APPEND CMAKE_PROJECT_TOP_LEVEL_INCLUDES "${CMAKE_TOOLCHAIN_FILE}")
+			list(APPEND CMAKE_TRY_COMPILE_PLATFORM_VARIABLES CMAKE_PROJECT_TOP_LEVEL_INCLUDES)
         endif()
     endif()
 endmacro()
 
 macro(fetch_vcpkg)
 	include(gitect)
-	if(GIT_EXECUTABLE)
-		execute_process(
-			COMMAND ${GIT_EXECUTABLE} clone https://github.com/microsoft/vcpkg
-				WORKING_DIRECTORY $ENV{USERPROFILE}$ENV{HOME}
-			)
-		execute_process(
-			COMMAND ${CMAKE_COMMAND} --build .
-			WORKING_DIRECTORY ${vcpkg_SOURCE_DIR}
-		)
-	endif()
+	include(FetchContent)
+	FetchContent_Populate(	VCPKG
+		GIT_REPOSITORY https://github.com/microsoft/vcpkg.git
+		GIT_TAG        703a8113f42e794534df5dfc5cece5dabcb949d0
+		SOURCE_DIR     "$ENV{USERPROFILE}$ENV{HOME}/vcpkg"
+	)
+
+	# if(GIT_EXECUTABLE)
+	# 	execute_process(
+	# 		COMMAND ${GIT_EXECUTABLE} clone https://github.com/microsoft/vcpkg
+	# 			WORKING_DIRECTORY $ENV{USERPROFILE}$ENV{HOME}
+	# 		)
+	# 	execute_process(
+	# 		COMMAND ${CMAKE_COMMAND} --build .
+	# 		WORKING_DIRECTORY ${vcpkg_SOURCE_DIR}
+	# 	)
+	# endif()
 endmacro()
 
-if(NOT ${VCPKG_FOUND})
+if(NOT VCPKG_FOUND)
 	find_vcpkg()
 endif()
 
