f2ab157a732479f190fa52bac2341a098121095d
Sergii Kryvonos <sergeikrivonos@gmail.com>
2024-11-22 01:19:35 +0100

    Add measure functions

--- omnn/rt/diag.hpp
+++ omnn/rt/diag.hpp
@@ -1,24 +1,35 @@
 
 #include <chrono>
 
-namespace omnn {    
-namespace measure {
+namespace omnn::measure {
 
-    template<typename F, typename ...Args>
-    static auto time(F&& func, Args&&... args)
-    {
-        auto start = std::chrono::steady_clock::now();
-        std::forward<decltype(func)>(func)(std::forward<Args>(args)...);
-        auto duration = std::chrono::steady_clock::now() - start;
-        return duration;
-    }
+template <typename F, typename... Args>
+static auto time(F&& func, Args&&... args) {
+    auto start = std::chrono::steady_clock::now();
+    std::forward<decltype(func)>(func)(std::forward<Args>(args)...);
+    auto duration = std::chrono::steady_clock::now() - start;
+    return duration;
+}
 
-    template<typename ...Args>
-    static auto nanoseconds(Args&&... args)
-    {
-        auto duration = time(std::forward<Args>(args)...);
-        auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
-        return ns.count();
-    }
-    
-}}
+template <typename... Args>
+static auto nanoseconds(Args&&... args) {
+    auto duration = time(std::forward<Args>(args)...);
+    auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
+    return ns.count();
+}
+
+template <typename... Args>
+static auto microseconds(Args&&... args) {
+    auto duration = time(std::forward<Args>(args)...);
+    auto ms = std::chrono::duration_cast<std::chrono::microseconds>(duration);
+    return ms.count();
+}
+
+template <typename... Args>
+static auto mills(Args&&... args) {
+    auto duration = time(std::forward<Args>(args)...);
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+    return ms.count();
+}
+
+} // namespace omnn::measure
