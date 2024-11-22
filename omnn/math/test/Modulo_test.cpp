f67950c1c0fea5c6f6a8211a69c2e9791e736f0c
Sergii Kryvonos <sergeikrivonos@gmail.com>
2024-11-22 16:47:44 +0100

    Modulo_Ordering_test

--- omnn/math/test/Modulo_test.cpp
+++ omnn/math/test/Modulo_test.cpp
@@ -121,3 +121,9 @@ BOOST_AUTO_TEST_CASE(ModuloExponentiationOptimization_test, *disabled())
     expr2.optimize();
     BOOST_TEST(expr2 == -888 % x);
 }
+
+BOOST_AUTO_TEST_CASE(Modulo_Ordering_test) {
+    auto _1 = "(1 % ((Y ^ 4)))"_v;
+    auto _2 = "((-4 * (Y ^ 3)) % ((Y ^ 4)))"_v;
+    InequalOrderCheck(_1, _2);
+}
