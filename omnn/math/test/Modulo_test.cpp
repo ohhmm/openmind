7b99214381f79c6f6224df52b9bf66e04c93fb78
Sergii Kryvonos <sergeikrivonos@gmail.com>
2024-11-27 18:02:27 +0100

    Add NegativeFractionModulo_test scenario

--- omnn/math/test/Modulo_test.cpp
+++ omnn/math/test/Modulo_test.cpp
@@ -28,6 +28,16 @@ BOOST_AUTO_TEST_CASE(NegativeFractionModulo_test, *disabled()) {
     auto _1 = "((((-2165) / 4)) % (-770))"_v;
     auto _2 = "-541.25"_v;
     BOOST_TEST(_1 == _2);
+
+    _1 = -500;
+    _2 = 700;
+    auto mod = _1 % _2;
+    BOOST_TEST(mod == 200);
+
+    _1 = -500;
+    _2 = -700;
+    mod = _1 % _2;
+    BOOST_TEST(mod == _1);
 }
 
 BOOST_AUTO_TEST_CASE(ModIntLess_comparator_test) {
