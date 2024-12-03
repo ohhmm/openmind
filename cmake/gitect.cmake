9bb13c801f1874b05771fc2798f3cd776343d13b
Sergii Kryvonos <sergeikrivonos@gmail.com>
2024-12-03 01:04:24 +0100

    git targets: ignore unreachable remotes while fetching

--- cmake/gitect.cmake
+++ cmake/gitect.cmake
@@ -28,9 +28,9 @@ macro(add_git_target)
 	set(new_target ${ARGV0})
 	list(POP_FRONT args)
 	add_git_target_multiple_commands(${new_target}
-		COMMAND ${GIT_EXECUTABLE} fetch --all
+		COMMAND ${GIT_EXECUTABLE} fetch --all || echo git fetch error
 		COMMAND ${GIT_EXECUTABLE} ${args}
-		COMMAND ${GIT_EXECUTABLE} fetch --all
+		COMMAND ${GIT_EXECUTABLE} fetch --all || echo git fetch error
 		)
 endmacro()
 
@@ -38,7 +38,7 @@ macro(add_force_push_head_to_develop_target)
 	set(new_target force-push-head${ARGV0}-to-develop)
 	add_custom_target(${new_target}
 		COMMAND ${GIT_EXECUTABLE} push origin HEAD~${ARGV0}:develop -f || ${GIT_EXECUTABLE} push origin HEAD~${ARGV0}:refs/heads/develop
-		COMMAND ${GIT_EXECUTABLE} fetch --all
+		COMMAND ${GIT_EXECUTABLE} fetch --all || echo git fetch error
 		)
 	set_target_properties(${new_target} PROPERTIES
 		EXCLUDE_FROM_ALL 1
@@ -63,7 +63,7 @@ if(GIT_EXECUTABLE)
 		COMMAND ${GIT_EXECUTABLE} rebase HEAD develop --autostash || ${GIT_EXECUTABLE} rebase --continue
 		COMMAND ${GIT_EXECUTABLE} pull --rebase --autostash origin main || ${GIT_EXECUTABLE} rebase --continue
 		COMMAND ${GIT_EXECUTABLE} rebase -i --autostash origin/main || ${GIT_EXECUTABLE} rebase --continue
-		COMMAND ${GIT_EXECUTABLE} fetch --all
+		COMMAND ${GIT_EXECUTABLE} fetch --all || echo git fetch error
 	)
 	set_target_properties(bring-to-develop-interactive PROPERTIES FOLDER "util/git/develop")
 
@@ -73,7 +73,7 @@ if(GIT_EXECUTABLE)
 			COMMAND ${GIT_EXECUTABLE} pull --rebase --autostash || ${GIT_EXECUTABLE} pull --rebase --autostash origin HEAD
 			COMMAND ${GIT_EXECUTABLE} pull --rebase --autostash origin HEAD
 			COMMAND ${GIT_EXECUTABLE} pull --rebase --autostash https://github.com/ohhmm/openmind HEAD
-			COMMAND ${GIT_EXECUTABLE} fetch --all
+			COMMAND ${GIT_EXECUTABLE} fetch --all || echo git fetch error
 		)
 		set_target_properties(update-openmind PROPERTIES
 			EXCLUDE_FROM_ALL 1
@@ -113,7 +113,7 @@ if(GIT_EXECUTABLE)
 	add_custom_target(push-to-develop
 		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 		COMMAND ${GIT_EXECUTABLE} push origin HEAD:develop || ${GIT_EXECUTABLE} push origin HEAD:refs/heads/develop
-		COMMAND ${GIT_EXECUTABLE} fetch --all
+		COMMAND ${GIT_EXECUTABLE} fetch --all || echo git fetch error
 	)
 	set_target_properties(push-to-develop PROPERTIES
 		EXCLUDE_FROM_ALL 1
@@ -141,7 +141,7 @@ if(GIT_EXECUTABLE)
 		)
 	else()
 		add_custom_target(rebase-all-branches
-			COMMAND ${GIT_EXECUTABLE} fetch --all
+			COMMAND ${GIT_EXECUTABLE} fetch --all || echo git fetch error
 			COMMAND ${CMAKE_COMMAND} -E echo "Rebasing all branches onto main"
 			COMMAND ${CMAKE_COMMAND} -E env bash "${CMAKE_SOURCE_DIR}/cmake/rebase-all-branches.sh"
 			WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
