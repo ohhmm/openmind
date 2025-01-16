7dd47e19d010f7c4a82739fa4f263ba5a04a291d
Sergii Kryvonos <sergeikrivonos@gmail.com>
2025-01-16 19:49:58 +0100

    Devin-fix test RadicalSolving_test

--- omnn/math/test/Radicals.cpp
+++ omnn/math/test/Radicals.cpp
@@ -87,7 +87,7 @@ BOOST_AUTO_TEST_CASE(RadicalExponentiationExpressions_test, *disabled()) {
     BOOST_TEST(_2 == 6);
 }
 
-BOOST_AUTO_TEST_CASE(RadicalSolving_test, *disabled()) {
+BOOST_AUTO_TEST_CASE(RadicalSolving_test) {
     Valuable _1("sqrt(8-(x^2))-4");
     auto solutions = _1.Solutions();
     BOOST_TEST(solutions.size() == 2);
